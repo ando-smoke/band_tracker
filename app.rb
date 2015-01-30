7033f78a0ef0e50cfd83644a0bef9095b9072e9d
Andy Uppendahl <a.uppendahl@gmail.com>
2015-01-30 14:33:19 -0800

    Add functionality to add new venues into system.

--- app.rb
+++ app.rb
@@ -5,3 +5,14 @@ Dir[File.dirname(__FILE__) + "/lib/*.rb"].each { |file| require file }
 get("/") do
   erb(:index)
 end
+
+get("/venues") do
+  @venues = Venue.all()
+  erb(:venues)
+end
+
+post("/venues") do
+  venue_name = params.fetch("venue_name")
+  Venue.create({ :name => venue_name })
+  redirect("/venues")
+end
